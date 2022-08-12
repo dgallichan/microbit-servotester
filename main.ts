23f9f8a129cf31098cb7b95e5a0e03b9b570973e
Daniel Gallichan <dgallichan@yahoo.com>
2022-08-12 16:27:51 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onButtonPressed(Button.A, function () {
+    isLive = true
     basic.showLeds(`
         # . . . #
         . # # # .
@@ -24,7 +25,9 @@ input.onButtonPressed(Button.A, function () {
         . . . # .
         # # # # .
         `)
+    isLive = false
 })
+let isLive = false
 basic.showLeds(`
     . # # # #
     . # . . .
@@ -32,7 +35,11 @@ basic.showLeds(`
     . . . # .
     # # # # .
     `)
+isLive = true
 basic.forever(function () {
-    pins.servoWritePin(AnalogPin.P0, Math.map(input.rotation(Rotation.Pitch), 0, 90, 0, 180))
-    pins.servoWritePin(AnalogPin.P1, Math.map(input.rotation(Rotation.Roll), 0, 90, 0, 180))
+    if (isLive) {
+        pins.servoWritePin(AnalogPin.P0, Math.map(input.rotation(Rotation.Pitch), 0, 90, 0, 180))
+        pins.servoWritePin(AnalogPin.P1, Math.map(input.rotation(Rotation.Roll), 0, 90, 0, 180))
+    }
+    basic.pause(100)
 })
