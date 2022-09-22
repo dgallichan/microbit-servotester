16799f794696956f41017e43261965657b50f865
Daniel Gallichan <dgallichan@yahoo.com>
2022-09-22 12:04:30 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-input.onButtonPressed(Button.A, function () {
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     isLive = false
     basic.showLeds(`
         # . . . #
@@ -46,8 +46,14 @@ basic.showLeds(`
 isLive = true
 basic.forever(function () {
     if (isLive) {
-        pins.servoWritePin(AnalogPin.P0, Math.map(input.rotation(Rotation.Pitch), 0, 90, 0, 180))
-        pins.servoWritePin(AnalogPin.P1, Math.map(input.rotation(Rotation.Roll), 0, 90, 0, 180))
+        if (input.buttonIsPressed(Button.A)) {
+            pins.servoWritePin(AnalogPin.P0, Math.map(input.rotation(Rotation.Pitch), -90, 90, 0, 180))
+            pins.servoWritePin(AnalogPin.P1, Math.map(input.rotation(Rotation.Roll), -90, 90, 0, 180))
+        } else if (input.buttonIsPressed(Button.B)) {
+            pins.servoWritePin(AnalogPin.P0, 90)
+            pins.servoWritePin(AnalogPin.P1, 90)
+            pins.servoWritePin(AnalogPin.P2, 90)
+        }
     }
     basic.pause(100)
 })
